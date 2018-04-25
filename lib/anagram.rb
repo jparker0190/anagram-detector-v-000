605496bf46f906870452920e558753c4cca9ea25
Joseph Parker <jparker010190@gmail.com>
2018-04-25 23:43:41 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,9 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  def intialize(word)
+    word_array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
+  end
+end
